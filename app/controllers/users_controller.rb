87ab09d3158269384c284eb38f15a022d4a3461a
Nurfarrah 'Ain Amran <nurfarrahain93@gmail.com>
2014-11-07 02:33:57 +0000

    fixed error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,7 +4,8 @@ class UsersController < ApplicationController
     end
     
     def create
-        @user = User.new
+        @user = User.new(user_params)
+        
         
         if @user.save
             redirect_to root_path, flash: {success:  'Successfully created account!' }
